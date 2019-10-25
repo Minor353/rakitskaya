07ffb03a96f1cc3d2da301785469b23d35e560e8
Hennadii Nikitenko <vankert353@gmail.com>
2019-10-25 18:36:27 +0300

    Адаптация до 1024

--- app/blocks/components/about-main/about-main.js
+++ app/blocks/components/about-main/about-main.js
@@ -15,6 +15,31 @@ export default function reviews() {
 		dots: false,
 		arrows: false,
 		centerMode: false,
-		focusOnSelect: true
+		focusOnSelect: true,
+		responsive: [{
+				breakpoint: 1030,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
 	});
 }
